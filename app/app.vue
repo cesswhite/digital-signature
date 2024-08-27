4b4a9d2169ae622f5258b6e2b4268e463f34e637
César Armando <44076866+cesswhite@users.noreply.github.com>
2024-08-27 16:33:51 -0600

    [ADD] - Add SEO Meta data

--- app/app.vue
+++ app/app.vue
@@ -4,3 +4,18 @@
     <AppFooter />
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Signature | Digital Signature in Seconds',
+  description: 'Create digital signatures effortlessly. Generate, customize, and download your digital signature instantly using our intuitive canvas interface. No need for complex setups or external services - everything happens right in your browser.',
+  ogTitle: 'Signature | Digital Signature in Seconds',
+  ogDescription: 'Create digital signatures effortlessly. Generate, customize, and download your digital signature instantly using our intuitive canvas interface. No need for complex setups or external services - everything happens right in your browser.',
+  ogImage: 'https://res.cloudinary.com/dpvsklksg/image/upload/v1724796869/Captura_de_pantalla_2024-08-27_a_la_s_4.13.32_p.m._tlwjj4.png',
+  ogUrl: 'https://res.cloudinary.com/dpvsklksg/image/upload/v1724796869/Captura_de_pantalla_2024-08-27_a_la_s_4.13.32_p.m._tlwjj4.png',
+  ogType: 'website',
+  ogSiteName: 'Signature | Digital Signature in Seconds',
+  ogImageWidth: 1200,
+  ogImageHeight: 630
+});
+</script>
